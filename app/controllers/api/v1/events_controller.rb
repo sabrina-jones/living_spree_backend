4d5b9394718860efaebea00fa5bd44cade4d068a
sabrina-jones <sabrina.jones87@gmail.com>
2021-06-21 00:22:31 -0500

    categories and events controllers, nested routes

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,2 +1,40 @@
 class Api::V1::EventsController < ApplicationController
+
+    before_action :set_category
+    
+    #all events of a specific category
+    def index
+        @events = @category.events
+        render json: @events  
+    end 
+
+    def show
+        @event = Event.find(params[:id])
+        render json: @event
+    end
+
+    def create
+        @event = @category.events.new(event_params)
+        if @event.save
+            render json: @event
+        else
+            render json: {error: "Error creating event"}
+        end
+    end 
+
+    def destroy
+        @event = Event.find(params[:id])
+        @event.destroy
+    end 
+
+    private
+
+    def set_category
+        @category = Category.find(params[:category_id])
+    end
+
+    def event_params
+        params.require(:event).permit(:category_id, :image_url, :name, :summary, :when, :time, :where)
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :events
-      resources :categories
+      #nested so can only access the events route through categories
+      resources :categories do
+        resources :events
+      end
     end
   end
 end
