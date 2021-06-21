22b9ebfbec6cfe0a6be4644bb65787693f7414b7
sabrina-jones <sabrina.jones87@gmail.com>
2021-06-21 18:48:21 -0500

    another event to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ concerts = Category.create(name: "Concerts")
 festivals = Category.create(name: "Festivals")
 shows = Category.create(name: "Shows")
 
-Event.create(
+event = Event.create(
     category_id: concerts.id,
     image_url: "https://assets.simpleviewinc.com/simpleview/image/fetch/c_fill,h_286,q_75,w_357/https://assets.simpleviewinc.com/simpleview/image/upload/crm/kansascityks/MPAC_-_Styx_397A9FC1-5056-B3A8-49DF6874CE44E46C_3984e9a6-5056-b3a8-49afa76c209d95e2.jpg",
     name: "Styx",
@@ -18,3 +18,13 @@ Event.create(
     when: "Fri, Jun 25",
     time: "7 – 11 PM",
     where: "Azura Amphitheater 633 N 130th St, Bonner Springs, KS")
+
+
+event_two = Event.create(
+    category_id: concerts.id,
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4yDfsDIwicd-12W6lc7uV4gcmZONtLhjGc-lKIbHZFg&s=10",
+    name: "The Weekend",
+    summary: "Buy The Weeknd, T-Mobile Center Tickets for Sat Feb 19 2022",
+    when: "Sat, Feb 19",
+    time: "TBD",
+    where: "T-Mobile Center")
